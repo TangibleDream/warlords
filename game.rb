29d5ed3330ed491f75136a1000c045dd65c8c01e
TangibleDream <darclan28@yahoo.com>
2014-10-21 16:40:12 -0500

    Added splash and start og game class

---
+++ game.rb
@@ -0,0 +1,10 @@
+class Game
+  def initialize
+    puts "1. New Game"
+    puts "2. Continue Game"
+    newgame = gets.chomp.to_i
+  end
+  def run
+
+  end
+end
---
+++ splash.rb
@@ -0,0 +1,4 @@
+def splash(game, coder, start_date)
+  puts "#{game}"
+  puts "Written by #{coder} on #{start_date}"
+end
