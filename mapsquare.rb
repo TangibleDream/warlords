ce4798968cf830ad9e137085beeca9edcf1dd787
TangibleDream <darclan28@yahoo.com>
2014-10-21 22:34:36 -0500

    mapsuare class added

---
+++ mapsquare.rb
@@ -0,0 +1,16 @@
+class Mapsquare
+  def initialize(terrain, x, y)
+    @terrain = terrain
+    @x = x
+    @y = y
+  end
+  def get_terrain
+    @terrain
+  end
+  def get_x
+    @x
+  end
+  def get_y
+    @y
+  end
+end
