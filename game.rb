c6fc7a2833c9e57a254ecbeffa17581c6b1c0d60
TangibleDream <darclan28@yahoo.com>
2014-10-25 17:33:46 -0500

    Nice Map refactor

--- game.rb
+++ game.rb
@@ -12,23 +12,6 @@ class Game
   end
   def create_game
     map = Map.new(16)
-    x = 0
-    y = 0
-    ma = []
-    while x < 4
-      y = 0 if y > 3
-      ma << Mapsquare.new('grass', x, y)
-      y = y + 1
-      if y > 3
-        y = 0
-        x = x + 1
-      end
-    end
-    ma[5].set_terrain('castle')
-    ma[6].set_terrain('castle')
-    ma[9].set_terrain('castle')
-    ma[10].set_terrain('castle')
-    map.set_map(ma)
   end
   def run
   end
--- map.rb
+++ map.rb
@@ -3,12 +3,47 @@ class Map
     @side = Math.sqrt(squares).to_i
     @squares = squares
     @map_array = []
+    if squares == 16
+      x = 0
+      y = 0
+      while x < 4
+        y = 0 if y > 3
+        @map_array << Mapsquare.new('grass', x, y)
+        y = y + 1
+        if y > 3
+          y = 0
+          x = x + 1
+        end
+      end
+      @map_array[5].set_terrain('castle')
+      @map_array[6].set_terrain('castle')
+      @map_array[9].set_terrain('castle')
+      @map_array[10].set_terrain('castle')
+      savemap(@map_array)
+    end
   end
   def get_squares
     @squares
   end
   def get_side
     @side
+  end
+  def savemap(map)
+    fileout = File.open('map.json', 'w')
+    fileout.write"{\n"
+    fileout.write"   \"map\" :\n"
+    fileout.write"      {\n"
+    x = 1
+    map.each do |mapsqr|
+      terrain = "G" if mapsqr.get_terrain == 'grass'
+      terrain = "C" if mapsqr.get_terrain == 'castle'
+      fileout.write"         \"#{x.to_s}\" : \"#{terrain}\",\n" if x < map.count
+      fileout.write"         \"#{x.to_s}\" : \"#{terrain}\"\n" if x == map.count
+      x = x + 1
+    end
+    fileout.write"      }\n"
+    fileout.write"}"
+    fileout.close
   end
   def set_map(arr)
     @map_array = arr
